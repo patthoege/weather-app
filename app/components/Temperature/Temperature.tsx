39d13144bc978de35a887968ad240eaabe830258
Patricia Höge <paty.hoge98@gmail.com>
2024-07-04 13:15:34 +0200

    Add Temperature component with global context and conditional rendering

--- app/components/Temperature/Temperature.tsx
+++ app/components/Temperature/Temperature.tsx
@@ -1,9 +1,25 @@
 "use client";
+import { useGlobalContext } from "@/app/context/GlobalContext";
 import React from "react";
 
 
 function Temperature() {
-    return <div>Temperature</div>;
+    const { forecast } = useGlobalContext();
+    const { main, timezone, name, weather } = forecast;
+
+    console.log(main)
+
+    if(!forecast || !weather) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+    <div className="pt-6 pb-5 border border-gray-400 rounded-lg flex flex-col justify-between
+     dark:bg-dark-grey dark:border-gray-400 shadow-md dark:shadow-none"
+    >
+        Temperature
+    </div>
+    );
 }
 
 export default Temperature;
