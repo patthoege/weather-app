5d43af7b2805e44dd8d4a33ea5987ab4fbc75b7f
Patricia Höge <paty.hoge98@gmail.com>
2024-07-04 09:29:38 +0200

    created defaultStates file in utils

---
+++ app/utils/defaultStates.tsx
@@ -0,0 +1,39 @@
+const defaultStates = [
+    {
+        name: "Munich",
+        country: "GY",
+        state: "Bayern",
+        lat:  48.1351,
+        long:  11.5820,
+    },
+    {
+        name: "New York",
+        country: "US",
+        state: "New York",
+        lat: 40.7128,
+        long: -74.0060,
+    },
+    {
+        name: "Sydney",
+        country: "AU",
+        state: "New South Wales",
+        lat: -33.8688,
+        long: 151.2093,
+    },
+    {
+        name: "London",
+        country: "GB",
+        state: "Greater London",
+        lat: 51.5074,
+        long: -0.1278,
+    },
+    {
+        name: "Madrid",
+        country: "ES",
+        state: "Madrid",
+        lat: 40.4165,
+        long: -3.7026,
+    }
+]
+
+export default defaultStates;
