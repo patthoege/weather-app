9f345c6bb5c84491c9b7b6d272c3559b333ce9e0
Patricia Höge <paty.hoge98@gmail.com>
2024-07-09 07:40:53 +0200

    Created UvIndex component and set layout uvIndndexMax and uvIndexCategory information

---
+++ app/components/UvIndex/UvIndex.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { useGlobalContext } from "@/app/context/GlobalContext";
+import { sun } from "@/app/utils/Icons";
+import { Skeleton } from "@/components/ui/skeleton";
+import React from "react";
+
+function UvIndex() {
+    const { uvIndex } = useGlobalContext();
+
+    // check if uv index data is available,
+    //check if necessary properties are available
+    if (
+        !uvIndex ||
+        !uvIndex.daily
+    ) {
+        return <Skeleton className="h-[12rem] w-full" />;
+    }
+
+    const { daily } = uvIndex;
+    const { uv_index_clear_sky_max, uv_index_max } = daily;
+    const uvIndexMax = uv_index_max[0].toFixed(0);
+
+    const uvIndexCategory = (uvIndex: number) => {
+        if (uvIndex <= 2) {
+          return {
+            text: "Low",
+            protection: "No protection required",
+          };
+        } else if (uvIndex <= 5) {
+          return {
+            text: "Moderate",
+            protection: "Stay in shade near midday.",
+          };
+        } else if (uvIndex <= 7) {
+          return {
+            text: "High",
+            protection: "Wear a hat and sunglasses.",
+          };
+        } else if (uvIndex <= 10) {
+          return {
+            text: "Very High",
+            protection: "Apply sunscreen SPF 30+ every 2 hours.",
+          };
+        } else if (uvIndex > 10) {
+          return {
+            text: "Extreme",
+            protection: "Avoid being outside.",
+          };
+        } else {
+          return {
+            text: "Extreme",
+            protection: "Avoid being outside.",
+          };
+        }
+      };
+
+  return (
+    <div className="pt-6 pb-5 px-4 h-[12rem] border border-gray-400 
+    rounded-lg flex flex-col gap-8 dark:bg-dark-grey shadow-sm dark:shadow-none">
+        <div className="top ">
+            <h2 className="flex items-center gap-2 font-medium">{sun}Uv Index</h2>
+            <p className="pt-4 text-2xl">
+                {uvIndexMax}
+            <span className="text-sm">
+                ({uvIndexCategory(uvIndexMax).text})
+            </span>
+            </p>
+           
+        </div>
+        <p className="text-sm">
+            {uvIndexCategory(uvIndexMax).protection}
+        </p>
+    </div>
+  )
+}
+
+export default UvIndex;
