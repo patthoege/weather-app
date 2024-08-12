8c35435fe039aff81c46ee23ab4df86bc48f6a98
Patricia Höge <paty.hoge98@gmail.com>
2024-08-12 12:45:58 +0200

    Added getClickedState functionality to the defaultStates

--- app/components/TopCities/TopCities.tsx
+++ app/components/TopCities/TopCities.tsx
@@ -1,9 +1,16 @@
 "use client";
 
+import { useGlobalContextUpdate } from '@/app/context/GlobalContext';
 import defaultStates from '@/app/utils/defaultStates';
 import React from 'react'
 
 function TopCities() {
+  const { setActiveCityCoords } = useGlobalContextUpdate();
+
+  const getClickedState = (state: typeof defaultStates[number]) => {
+    setActiveCityCoords([state.lat, state.lon]);
+  };
+
   return (
     <div>
         <h2 className="flex items-center gap-2 font-medium">
@@ -14,7 +21,8 @@ function TopCities() {
                 return (
                 <div
                  className="border border-gray-400 rounded-lg cursor-pointer dark:bg-dark-grey shadow-sm dark:shadow-none"
-                 key={index} 
+                 key={index}
+                 onClick={() => getClickedState(state)}
                 >
                    <p className="px-6 py-4">{state.name}</p> 
                 </div>
